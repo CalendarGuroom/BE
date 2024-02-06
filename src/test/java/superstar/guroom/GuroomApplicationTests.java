57e55f71a987a6e47e0820f4c9cd6ee1a03c9929
eundeang <111877048+eundeang@users.noreply.github.com>
2024-02-06 10:53:12 +0800

    init: 프로젝트 파일 생성

---
+++ src/test/java/superstar/guroom/GuroomApplicationTests.java
@@ -0,0 +1,13 @@
+package superstar.guroom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GuroomApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
