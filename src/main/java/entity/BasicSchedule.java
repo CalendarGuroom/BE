c0df32625018e2bc8d77e7d65ec5009f07dc932a
eundeang <111877048+eundeang@users.noreply.github.com>
2024-02-13 17:03:20 +0800

    feat: User, BasicSchedule, Calender Entity 구현

---
+++ src/main/java/entity/BasicSchedule.java
@@ -0,0 +1,51 @@
+package entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+
+@Entity
+@Getter
+@Table(name = "basic_schedule")
+public class BasicSchedule {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long scheduleId;
+
+    @Column
+    private String scheduleType;
+
+    @Column
+    private String scheduleTitle;
+
+    @Column
+    private Boolean scheduleAllTime;
+
+    @Column
+    private String scheduleStartDate;
+
+    @Column
+    private String scheduleStartTime;
+
+    @Column
+    private String scheduleEndDate;
+
+    @Column
+    private String scheduleEndTime;
+
+    @Column
+    private Boolean scheduleAlarm;
+
+    @Column
+    private String schedulePlace;
+
+    @Column
+    private String scheduleColor;
+
+    @Column
+    private Boolean scheduleComplete;
+
+
+
+
+
+}
